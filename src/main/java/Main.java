b8cb0e6fcaf4f464fdcb6165c670304ce2a232a1
liviorobojob <livio@robojob.be>
2015-03-18 12:46:33 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+	final Group root = new Group();
+
+	private final double sceneWidth = 1024;
+	private final double sceneHeight = 768;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Scene scene = new Scene(root, sceneWidth, sceneHeight, true);
+		scene.setFill(Color.GRAY);
+		Label label = new Label("Testing jenkins");
+		root.getChildren().add(label);
+		
+		primaryStage.setTitle("Jeninks Test");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+}
